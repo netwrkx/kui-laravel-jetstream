3329df5f9d90373fbc03df0e66bff3245ba2a45f
Adebowale <adebowale72@gmail.com>
2024-04-29 13:15:22 +0100

    updated app.js

--- stubs/custom/inertia/js/app.vite.js
+++ stubs/custom/inertia/js/app.vite.js
@@ -4,7 +4,7 @@ import '../css/app.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { isDark } from './Composables'
 import Toast from 'vue-toastification'
 
@@ -16,7 +16,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(ZiggyVue)
             .use(Toast, {
                 hideProgressBar: true,
                 closeOnClick: false,
